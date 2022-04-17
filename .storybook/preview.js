c21392daf2885c4bfc28458cafdea695e59168fa
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-04-17 14:55:02 -0300

    initial commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,26 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import GlobalStyles from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
 
 export const parameters = {
+  backgrounds: {
+    default: 'qurio-light',
+    values: [
+      {
+        name: 'qurio-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'qurio-dark',
+        value: theme.colors.mainBg
+      },
+      {
+        name: 'qurio-primary',
+        value: theme.colors.primary
+      }
+    ]
+  },
   nextRouter: {
     Provider: RouterContext.Provider
   }
@@ -9,9 +28,9 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
